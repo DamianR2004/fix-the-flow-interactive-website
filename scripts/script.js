95127ce94fe2dac62455639fde98629390bfeeaa
DamianR04 <damian.romeijn@hva.nl>
2023-12-14 20:04:33 +0100

    V0.9

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 //  PLUS button
 const plusButton = document.querySelector(".add-button");
-const deDialog = document.querySelector("dialog");
+const deDialog = document.querySelector("dialog:nth-of-type(1)");
 
 
 function openDialog() {
@@ -17,7 +17,35 @@ const hetMenu = document.querySelector(".hoofdmenu");
 
 function openMenu() {
     hetMenu.classList.toggle("open");
+    hetMenu.classList.toggle("close");
 }
 
 menuButton.addEventListener("click", openMenu);
+
+
+
+
+let contactbutton = document.querySelector(".contact-button");
+let contactdialog = document.querySelector(".contact-opnemen dialog");
+
+
+contactbutton.addEventListener("click", () => {
+    contactdialog.showModal();
+});
+
+
+
+let verstuurButton = document.querySelector("input submit");
+let versturenDialog = document.querySelector(".contact-opnemen dialog:nth-of-type(3)");
+
+
+verstuurButton.addEventListener("click", () => {
+    versturenDialog.showModal();
+});
+
+
+
+  window.onload = function() {
+    window.location.href = 'verzonden.html';
+  };
 
